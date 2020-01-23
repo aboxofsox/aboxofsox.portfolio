5edd1b20ecdd5d83b0b8ef11631a3b14998dba76
aboxofsox <idkwthfml@gmail.com>
2020-01-23 17:24:16 -0600

    removed javascript

--- public/js/modules/binds.module.js
+++
@@ -1,7 +0,0 @@
-const $ = document.querySelector.bind(document);
-const $$ = document.querySelector.bind(document);
-const $_ = document.getElementById.bind(document);
-const $$_ = document.getElementsByClassName.bind(document);
-const $$__ = document.getElementsByTagName.bind(document);
-
-export {$, $$, $_, $$_, $$__};
